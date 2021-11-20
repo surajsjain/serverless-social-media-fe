1db56685e5dacab2191bff0bf254a0f12a5e1cae
Suraj Shamsundar Jain <surajsjain@hotmail.com>
2021-11-20 13:02:26 -0600

    Linked create posts

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import {
 } from "react-router-dom";
 
 import Home from "./pages/home";
+import CreatePost from "./pages/create_post";
 
 export default function App() {
     var base_req_url = "https://servelesssocialmedia.surajsjn.workers.dev/";
 
     return (
-        <div style={{"padding": "2%", "font-family": "sans-serif"}}>
+        <div style={{"padding": "2%", fontFamily: "sans-serif"}}>
             <Router>
                 <Switch>
-                    <Route path="/">
+                    <Route exact path="/">
                         <Home base_req_url = {base_req_url}/>
+                    </Route>
+                    <Route exact path="/create">
+                        <CreatePost></CreatePost>
                     </Route>
                 </Switch>
             </Router>
