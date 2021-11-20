c1cc75b13e8c84360c8783a74d8091c8155ea0e5
Suraj Shamsundar Jain <surajsjain@hotmail.com>
2021-11-20 13:01:37 -0600

    Created a placeholder page

---
+++ src/pages/create_post.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CreatePost()
+{
+    return (
+        <div>
+            <h1>Create your own post</h1>
+        </div>
+    );
+}
+
+export default CreatePost;
